06ca595f2d6aafc92241239266f08755684ed108
sunlaud <sunlaud@mail.ru>
2017-05-22 23:03:10 +0300

    Removed commons-collections dependency

--- findticket-uz-client/src/main/java/io/github/sunlaud/findticket/client/uz/util/Utils.java
+++ findticket-uz-client/src/main/java/io/github/sunlaud/findticket/client/uz/util/Utils.java
@@ -7,14 +7,12 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.Iterables;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections.MapUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 public class Utils {
@@ -26,7 +24,9 @@ public class Utils {
         HashMap<String,Object> o = OBJECT_MAPPER.readValue(rawResponse, typeRef);
 
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        MapUtils.debugPrint(new PrintStream(bos), "response", o);
+        new PrintStream(bos).print(o);
+//        it is unwise to keep commons-collections only for pretty-print
+//        MapUtils.debugPrint(new PrintStream(bos), "response", o);
         log.debug(bos.toString("UTF-8"));
     }
 
