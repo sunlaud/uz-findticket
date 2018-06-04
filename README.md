7162b65b2c9f97290876c1f17f9f391340bc3a5d
sunlaud <sunlaud@mail.ru>
2018-06-05 01:25:33 +0300

    Added testcase to verify potential bug - error parsing daytime string containing friday in ukrainian (п'ятниця)

--- README.md
+++ README.md
@@ -20,3 +20,8 @@ java -jar findticket-cli-app/target/findticket-cli-app-1.0-SNAPSHOT-jar-with-dep
 1. ```mvn --batch-mode release:prepare```
 2. ```mvn  release:clean```
 3. ```git push --follow-tags```
+
+
+TODO:
+
+* possible bug - Jackson fails to parse day of week which has ukrainian symbol <'> e.g. <п'ятниця>, reproduced only on real device
---
+++ findticket-uz-client/src/test/java/io/github/sunlaud/findticket/client/uz/DateFormatTest.java
@@ -0,0 +1,39 @@
+package io.github.sunlaud.findticket.client.uz;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.joda.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.joda.ser.LocalDateSerializer;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class DateFormatTest {
+
+    public static class Dto {
+        @JsonDeserialize(using = LocalDateDeserializer.class)
+        @JsonSerialize(using = LocalDateSerializer.class)
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "EEEE, dd.MM.yyyy", locale = "uk")
+        @JsonProperty("date")
+        private LocalDate date;
+
+        public LocalDate getDate() {
+            return date;
+        }
+    }
+
+    @Test
+    public void objectMapperIsAbleToParseUkrainianDayOfWeek() throws IOException {
+        String json = "{ \"date\": \"п'ятниця, 01.06.2018\" }";
+
+        Dto dto = new ObjectMapper().readValue(json, Dto.class);
+
+        assertEquals(new LocalDate(2018, 6, 1), dto.getDate());
+    }
+}
